f2ac581ebd147f920195bfbb8244b2578173f110
Orji Dominion <130156592+dodosebn@users.noreply.github.com>
2024-05-09 16:48:36 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,30 +2,37 @@ document.addEventListener("DOMContentLoaded", function() {
     var display = document.getElementById("calcDisplay");
     var buttons = document.querySelectorAll(".calculator td button");
     var clearButton = document.querySelector(".clear");
-    
+
     buttons.forEach(function(button) {
         button.addEventListener("click", function() {
             var buttonText = this.textContent;
             var currentValue = display.value;
-            
+
             if (buttonText === "=") {
                 try {
-                    var result = eval(currentValue);
+                    var result = evaluateExpression(currentValue);
                     display.value = result;
                 } catch (error) {
                     display.value = "Error";
                 }
-            } else if (buttonText === ".") {
-                if (!currentValue.includes(".")) {
-                    display.value += buttonText;
-                }
+            } else if (buttonText === "C") {
+                display.value = "";
             } else {
                 display.value += buttonText;
             }
         });
     });
-    
+
+    function evaluateExpression(exp) {
+        exp = exp.replace(/×/g, "*");
+        exp = exp.replace(/÷/g, "/"); 
+
+        return eval(exp);
+    }
+
     clearButton.addEventListener("click", function() {
         display.value = display.value.slice(0, -1);
     });
 });
+
+
